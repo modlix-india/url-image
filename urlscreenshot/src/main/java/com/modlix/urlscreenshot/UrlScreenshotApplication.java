f78fd90832424f1f753b08f1a3d20ac24a708b0f
Kiran Kumar Grandhi <kiran.grandhi@gmail.com>
2024-12-29 08:41:57 +0530

    Readme added along with skeleton project

---
+++ urlscreenshot/src/main/java/com/modlix/urlscreenshot/UrlScreenshotApplication.java
@@ -0,0 +1,13 @@
+package com.modlix.urlscreenshot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlScreenshotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlScreenshotApplication.class, args);
+	}
+
+}
---
+++ urlscreenshot/src/test/java/com/modlix/urlscreenshot/UrlScreenshotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modlix.urlscreenshot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlScreenshotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
