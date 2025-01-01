a22570a7fb2f0837177d185f862d2f6a92ddb60e
serialcoder13 <kiran@modlix.com>
2025-01-01 16:21:17 +0530

    Moved from ehcache to caffine cache. Updated caching logic for diskbased

--- urlscreenshot/src/main/java/com/modlix/urlscreenshot/UrlScreenshotApplication.java
+++ urlscreenshot/src/main/java/com/modlix/urlscreenshot/UrlScreenshotApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @AutoConfiguration
 @EnableCaching
 @SpringBootApplication
+@EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
 public class UrlScreenshotApplication {
 
 	public static void main(String[] args) {
--- urlscreenshot/src/main/java/com/modlix/urlscreenshot/web/URL2ImageController.java
+++ urlscreenshot/src/main/java/com/modlix/urlscreenshot/web/URL2ImageController.java
@@ -2,9 +2,11 @@ package com.modlix.urlscreenshot.web;
 
 import java.io.IOException;
 
+import org.eclipse.persistence.annotations.DeleteAll;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,7 +34,41 @@ public class URL2ImageController {
         try {
             this.url2ImageService.get("url2image", request, response);
         } catch (Exception e) {
-            logger.error("Error while processing request", e);
+            logger.error("Error while creating the screenshot in get method : ", e);
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            try {
+                e.printStackTrace(response.getWriter());
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+    @DeleteMapping("/**")
+    public void delete(HttpServletRequest request, HttpServletResponse response) {
+
+        try {
+            this.url2ImageService.delete("url2image", request);
+            response.setStatus(HttpServletResponse.SC_OK);
+        } catch (Exception e) {
+            logger.error("Error while deleting from the cache in delete method : ", e);
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            try {
+                e.printStackTrace(response.getWriter());
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+    @DeleteMapping("/internal/all")
+    public void deleteAll(HttpServletRequest request, HttpServletResponse response) {
+
+        try {
+            this.url2ImageService.deleteAll();
+            response.setStatus(HttpServletResponse.SC_OK);
+        } catch (Exception e) {
+            logger.error("Error while deleting everything in cache in delete all method : ", e);
             response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
             try {
                 e.printStackTrace(response.getWriter());
